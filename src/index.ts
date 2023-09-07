7f3dece25391b2b9c2080ebd6f91622904697bd5
Anta <igorelyiw@gmail.com>
2023-09-07 14:31:23 +0300

    first commit

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import express, { Response, Request } from "express";
+
+const app = express();
+const port = 3000;
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("test!!!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
